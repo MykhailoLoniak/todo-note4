53801c4d827392fee3213d47c628c2d7c1acd16a
MykhailoLoniak <139628544+MykhailoLoniak@users.noreply.github.com>
2023-09-07 21:28:54 +0200

    .

--- src/components/arrData.jsx
+++ src/components/arrData.jsx
@@ -1,18 +1,33 @@
 const arrData = [
   {
-    date: new Date(1995, 8, 18),
-    name: ' Михайло Лоняк',
+    date: new Date(2014, 2, 17),
+    name: ' Олександр Лоняк',
     id: 1,
   },
   {
-    date: new Date(1995, 8, 1),
+    date: new Date(1995, 8, 18),
     name: ' Михайло Лоняк',
     id: 2,
   },
   {
-    date: new Date(1995, 8, 5),
-    name: ' Михайло Лоняк',
+    date: new Date(1996, 8, 30),
+    name: ' Віра Лоняк',
     id: 3,
+  },
+  {
+    date: new Date(1994, 4, 8),
+    name: ' Софія Бандаровська',
+    id: 4,
+  },
+  {
+    date: new Date(2021, 11, 6),
+    name: ' Катерина Бандаровська',
+    id: 5,
+  },
+  {
+    date: new Date(2021, 8, 1),
+    name: ' Катерина Бандаровська',
+    id: 5,
   },
 ];
 
--- src/components/birthdayTracker/BirthdayList.jsx
+++ src/components/birthdayTracker/BirthdayList.jsx
@@ -1,48 +1,68 @@
+import React from 'react';
 import { MdOutlineDeleteForever, MdOutlineEdit } from 'react-icons/md';
 import styles from '../../css/birthdayTracker/birthdayList.module.css';
 
 function BirthdayList(props) {
   const { arrBirthday, setArrBirthday } = props;
-  const handleDelete = (i) => {
-    const newArrBirthday = arrBirthday.filter((e) => i !== e.id);
-    // const newArrBirthday = arrBirthday.slice(i, 1);
+
+  const handleDelete = (id) => {
+    const newArrBirthday = arrBirthday.filter((item) => item.id !== id);
     setArrBirthday(newArrBirthday);
   };
+
+  const currentDate = new Date();
+
+  // Divide the birthdays into two arrays: past and future
+  const pastBirthdays = [];
+  const futureBirthdays = [];
+
+  arrBirthday.forEach((item) => {
+    if (item.date.getMonth() === currentDate.getMonth()) {
+      if (item.date.getDate() < currentDate.getDate()) {
+        pastBirthdays.push(item);
+      } else {
+        futureBirthdays.push(item);
+      }
+    } else if (item.date.getMonth() < currentDate.getMonth()) {
+      pastBirthdays.push(item);
+    } else {
+      futureBirthdays.push(item);
+    }
+  });
+
+  // Sort both arrays by month and day
+  pastBirthdays.sort((a, b) => {
+    const monthDayA = (a.date.getMonth() + 1) * 100 + a.date.getDate();
+    const monthDayB = (b.date.getMonth() + 1) * 100 + b.date.getDate();
+    return monthDayA - monthDayB;
+  });
+
+  futureBirthdays.sort((a, b) => {
+    const monthDayA = (a.date.getMonth() + 1) * 100 + a.date.getDate();
+    const monthDayB = (b.date.getMonth() + 1) * 100 + b.date.getDate();
+    return monthDayA - monthDayB;
+  });
+
+  // Combine the sorted arrays with past birthdays first
+  const sortedBirthdays = [...futureBirthdays, ...pastBirthdays];
+
   return (
-    <>
-      <ul>
-        {[...arrBirthday]
-          .sort((a, b) => {
-            const monthA = a.date.getMonth();
-            const monthB = b.date.getMonth();
-
-            // Отримуємо числа для порівняння
-            const dayA = a.date.getDate();
-            const dayB = b.date.getDate();
-
-            // Спочатку порівнюємо місяці, потім числа
-            if (monthA !== monthB) {
-              return monthA - monthB;
-            }
-            return dayA - dayB;
-          })
-          .map((item) => (
-            <li key={item.id}>
-              <div className={styles.date}>
-                {new Date(item.date).getDate()}.
-                {new Date(item.date).getMonth() + 1}.
-                {new Date(item.date).getFullYear()}{' '}
-              </div>{' '}
-              <div className={styles.text}> {item.name}</div>
-              <div className={styles.control}>
-                <MdOutlineEdit />
-                <MdOutlineDeleteForever onClick={() => handleDelete(item.id)} />
-              </div>
-              <div className='hr'></div>
-            </li>
-          ))}
-      </ul>
-    </>
+    <ul>
+      {sortedBirthdays.map((item) => (
+        <li key={item.id}>
+          <div className={styles.date}>
+            {item.date.getDate()}.{item.date.getMonth() + 1}.
+            {item.date.getFullYear()}
+          </div>
+          <div className={styles.text}>{item.name}</div>
+          <div className={styles.control}>
+            <MdOutlineEdit />
+            <MdOutlineDeleteForever onClick={() => handleDelete(item.id)} />
+          </div>
+          <div className='hr'></div>
+        </li>
+      ))}
+    </ul>
   );
 }
 
